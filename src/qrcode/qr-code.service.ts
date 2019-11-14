6a76e53ba94f697005196b1ad2777723cbd77c06
Lukasz Mazurowski <lukasz.mazurowski@gmail.com>
2019-11-14 11:33:23 +0100

    cr fixes

--- src/qrcode/qr-code.service.ts
+++ src/qrcode/qr-code.service.ts
@@ -2,22 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { toDataURL, QRCodeToDataURLOptions } from 'qrcode';
 import { QrCodeStyle } from './qr-code.interfaces';
 
+const defaultQrCodeStyle: QrCodeStyle = {
+  margin: 4,
+  color: {
+    dark: '#000',
+    light: '#FFF',
+  },
+};
+
 @Injectable()
 export class QrCodeService {
-  public async createQrCode(
-    content: string,
-    style?: QrCodeStyle,
-  ): Promise<string> {
-    const qrCodeStyles = Object.assign(
-      {
-        margin: 4,
-        color: {
-          dark: '#000',
-          light: '#FFF',
-        },
-      },
+  async createQrCode(content: string, style?: QrCodeStyle): Promise<string> {
+    const qrCodeStyles = {
+      defaultQrCodeStyle,
       style,
-    );
+    };
 
     const opts: QRCodeToDataURLOptions = {
       errorCorrectionLevel: 'H',
