26fe6ebb5e5b789c9d2096efa5d1eea5720ea4a0
Lukasz Mazurowski <lukasz.mazurowski@gmail.com>
2019-11-14 15:53:34 +0100

    added endpoint /app to serve qr code, removed tslint

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { QrCodeService } from './qrcode';
+import { ConfigService } from './config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly qrCodeService: QrCodeService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async getApplicationQrCode(): Promise<string> {
+    return await this.qrCodeService.createQrCode(
+      this.configService.ApplicationUrl,
+    );
   }
 }
