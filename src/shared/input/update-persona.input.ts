dadca28f333b1f20a060827bde1e6919b14355ff
Lukasz Mazurowski <lukasz.mazurowski@gmail.com>
2020-01-17 16:35:46 +0100

    - fixed update-persona.input.ts

--- src/shared/input/update-persona.input.ts
+++ src/shared/input/update-persona.input.ts
@@ -1,38 +1,19 @@
-import { IsString, IsOptional } from 'class-validator';
+import { IsOptional } from 'class-validator';
 import { Field, InputType } from 'type-graphql';
-import { GraphQLJSONObject } from 'graphql-type-json';
+import { CardInput } from './card.input';
+import { PageInput } from './page.input';
 
 @InputType()
 export class UpdatePersonaInput {
   @Field({
     nullable: true,
   })
-  @IsString()
   @IsOptional()
-  name?: string;
+  card?: CardInput;
 
   @Field({
     nullable: true,
   })
-  @IsString()
   @IsOptional()
-  description?: string;
-
-  @Field({
-    nullable: true,
-  })
-  @IsString()
-  @IsOptional()
-  logo?: string;
-
-  @Field({
-    nullable: true,
-  })
-  @IsString()
-  @IsOptional()
-  image?: string;
-
-  @Field(() => GraphQLJSONObject, { nullable: true })
-  @IsOptional()
-  details?: object;
+  page?: PageInput;
 }
