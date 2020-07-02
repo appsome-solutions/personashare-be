7a1a1166fc8b38e2edf4fb3085a3efeff443639a
Lukasz Mazurowski <36774684+lmazurowski@users.noreply.github.com>
2020-07-02 14:11:57 +0200

    Qr code issue + /qr endpoint + recommendation 4 week (#40)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,24 @@
 import {
   Controller,
   Get,
+  Post,
   Render,
   MethodNotAllowedException,
+  Body,
+  Res,
 } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
 import { QRCodeResponse } from './app.interfaces';
 import { ConfigService } from './config';
+import { QrCodeService } from './qrcode';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly configService: ConfigService,
+    private readonly qrCodeService: QrCodeService,
   ) {}
 
   @Get('/app')
@@ -21,6 +27,38 @@ export class AppController {
     if (this.configService.isDevEnv) {
       const qrCode = await this.appService.getApplicationQrCode();
       return { qrCode };
+    } else {
+      throw new MethodNotAllowedException();
+    }
+  }
+
+  @Get('/qr')
+  @Render('qrCodeCreate')
+  async createQrCode(@Body() payload: any): Promise<any> {
+    if (this.configService.isDevEnv) {
+      console.log(payload);
+      return {};
+    } else {
+      throw new MethodNotAllowedException();
+    }
+  }
+
+  @Post('/qr')
+  async handleQrCodePayload(
+    @Body() payload: any,
+    @Res() response: Response,
+  ): Promise<any> {
+    if (this.configService.isDevEnv) {
+      if (payload.link) {
+        const code = await this.qrCodeService.createQRCodeWithLogo(
+          payload.link,
+        );
+        response.set('Content-Type', 'application/octet-stream');
+        response.set('Content-Disposition', 'inline;filename=qrCode.svg');
+        response.send(code);
+      } else {
+        return { error: 'Missing link' };
+      }
     } else {
       throw new MethodNotAllowedException();
     }
--- src/persona/persona.service.ts
+++ src/persona/persona.service.ts
@@ -258,7 +258,7 @@ export class PersonaService {
       destinationKind: 'persona',
       recommendedTill: this.dayjs
         .utc()
-        .add(15, 'minute')
+        .add(4, 'week')
         .unix(),
     });
 
